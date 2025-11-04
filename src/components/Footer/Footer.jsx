0a1258a1bbfdef8ee4edcdfda692214d7428823c
Стас <marinewc@Noutbuk-Stas.local>
2025-11-04 20:25:15 +0200

    fix footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -27,7 +27,7 @@ export default async function Footer({ categories }) {
       </nav>
       <nav className={styles.footer__nav_list}>
         <h3 className={styles.footer__list_title}>
-          <NavigationLink title={'Контакты'} link="/Contact" />
+          <NavigationLink title={'Контакты'} link="/сontact" />
         </h3>
         <ul className={styles.footer__list}>
           {contacts.map((contact, i) => {
